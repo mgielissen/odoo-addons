e73ecfeac0326879bc98a94ff7aa6628d0e595ec
Mario Gielissen <mario@gielissen.org>
2016-02-18 16:17:56 +0100

    first commit

---
+++ website_admin_menu_v8/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Website Admin Menu for Odoo 8.0",
+    'version': "1.0",
+    'author': "Mario Gielissen",
+    'category': "Tools",
+    'summary':"View all website pages and sitemap.xml",
+    'data': [
+        'views/website_admin.xml',
+    ],
+    'demo': [],
+    'depends': ['website'],
+    'installable': True,
+}
